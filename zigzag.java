f9fe50330c99d405dbf2d37aeacee08a0e51c483
Abdul Majid <76196721+majid-2002@users.noreply.github.com>
2023-06-16 00:00:40 +0530

    udpate zigzag

--- zigzag.java
+++ zigzag.java
@@ -4,7 +4,7 @@ public class zigzag {
     public static String convert(String s, int numRows) {
         if (numRows == 1)
             return s;
-
+            
         StringBuilder res = new StringBuilder();
 
         for (int r = 0; r < numRows; r++) {
@@ -29,3 +29,28 @@ public class zigzag {
         System.out.println(res);
     }
 }
+
+// P A Y P A L I S H I R I N G
+
+// P     I     N
+// A   L S   I G
+// Y A   H	R
+// P     I
+
+// pseudocode:
+
+// 	if(numrows == 1) return s; // base case return the same value if the numrows is 1
+
+// 	res = ""
+
+// 	for r in range(numrows){ // loop in through each row
+// 		increment = (numrows - 1) * 2;
+// 		for(i = 0; i < r; i+= increment){
+// 			res += s[i]
+// 			if (r > 0 and r < numrows - 1 and 
+// 			i + increment - 2 * r < len(s)){
+// 			res += s[i + increment - 2 * r]
+// 			}
+// 		}
+// 	}
+		
