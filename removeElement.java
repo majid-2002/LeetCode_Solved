9a6a87f2f126b4384333c1b93f9ddf83f8e082d8
Abdul Majid <76196721+majid-2002@users.noreply.github.com>
2023-06-17 21:25:18 +0530

    remove a element

---
+++ removeElement.java
@@ -0,0 +1,43 @@
+public class removeElement {
+    public static int removeElement(int[] nums, int val) {
+        int k = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val)
+                nums[k++] = nums[i];
+        }
+
+        return k;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = { 0, 1, 2, 2, 3, 4 };
+        int val = 2;
+
+        int res = removeElement(nums, val);
+        System.out.println(res);
+
+    }
+}
+
+
+// 0, 1, 2, 2, 3, 4
+
+// i = 0 , k = 0;
+// 0, 1, 2, 2, 3, 4
+
+// i = 1 , k = 1;
+// 0, 1, 2, 2, 3, 4
+
+// i = 2, k = 2;
+// 0, 1, 2, 2, 3, 4
+
+// i = 3, k = 2;
+// 0, 1, 2, 2, 3, 4
+
+// i = 4, k = 2;
+// 0, 1, 3, 2, 3, 4
+
+// i = 5, k = 3;
+// 0, 1, 3, 4, 3, 4
+
+// i = 5, k = 4
